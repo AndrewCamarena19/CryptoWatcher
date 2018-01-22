3a42ae9b3f5b7d7805c7b9c2139d986a3985170e
Andy <andycamarena@ymail.com>
2018-01-21 17:09:45 -0800

    Using scalar factory to parse incoming html

--- app/src/main/java/com/andyisdope/cryptowatcher/Services/DataWebService.kt
+++ app/src/main/java/com/andyisdope/cryptowatcher/Services/DataWebService.kt
@@ -1,10 +1,12 @@
 package com.andyisdope.cryptowatcher.Services
 
 import com.andyisdope.cryptowatch.Currency
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.Url
 
 /**
@@ -13,19 +15,23 @@ import retrofit2.http.Url
 interface DataWebService {
 
     companion object {
-        val Image_Base_URL: String
-            get() = "https://www.cryptocompare.com/"
         val Data_Base_URL: String
             get() = "https://api.cryptowat.ch"
 
+        const val Init_URL = "https://coinmarketcap.com/all/views/all/"
+
         var retrofit
             get() = Retrofit.Builder()
-                    .baseUrl(Image_Base_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(Init_URL)
+                    //GsonConverterFactory.create()
+                    .addConverterFactory(ScalarsConverterFactory.create())
                     .build()
             set(value) = TODO()
     }
 
     @GET
     fun dataItems(@Url url: String): Call<Array<Currency>>
+
+    @GET(Init_URL)
+    fun getInitial(): Call<String>
 }
