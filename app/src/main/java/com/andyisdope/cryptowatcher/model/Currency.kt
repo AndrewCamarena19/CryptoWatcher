bf921ca0f83f696056268f85f43ad14128690203
Andy <andycamarena@ymail.com>
2018-01-22 14:38:06 -0800

    Change constructor signature

--- app/src/main/java/com/andyisdope/cryptowatcher/model/Currency.kt
+++ app/src/main/java/com/andyisdope/cryptowatcher/model/Currency.kt
@@ -6,14 +6,12 @@ import android.os.Parcelable
 /**
  * Created by Andy on 1/18/2018.
  */
-data class Currency(val Name: String, val ImageURL: String, val Symbol: String, val Place: Int,
-                        val FullName: String, val MarketCap: String, val CurrentPrice: String, val Change: String, val TimeFrame: String) : Parcelable {
+data class Currency(val Name: String, val Symbol: String, val Place: Int,
+                    val MarketCap: String, val CurrentPrice: String, val Change: String, val TimeFrame: String) : Parcelable {
     constructor(parcel: Parcel) : this(
             parcel.readString(),
             parcel.readString(),
-            parcel.readString(),
             parcel.readInt(),
-            parcel.readString(),
             parcel.readString(),
             parcel.readString(),
             parcel.readString(),
@@ -22,10 +20,8 @@ data class Currency(val Name: String, val ImageURL: String, val Symbol: String, 
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(Name)
-        parcel.writeString(ImageURL)
         parcel.writeString(Symbol)
         parcel.writeInt(Place)
-        parcel.writeString(FullName)
         parcel.writeString(MarketCap)
         parcel.writeString(CurrentPrice)
         parcel.writeString(Change)
