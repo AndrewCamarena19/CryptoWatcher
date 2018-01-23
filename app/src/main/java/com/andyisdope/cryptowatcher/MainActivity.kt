51be47d0a6c9bbec8606f80c73b8a9be00d6cfed
Andy <andycamarena@ymail.com>
2018-01-23 15:27:24 -0800

    Parsing input, all Sorting methods on fields, dialog selections

--- app/src/main/java/com/andyisdope/cryptowatcher/MainActivity.kt
+++ app/src/main/java/com/andyisdope/cryptowatcher/MainActivity.kt
@@ -21,17 +21,16 @@ import android.content.Intent
 import com.andyisdope.cryptowatcher.Adapters.CurrencyAdapter
 import com.andyisdope.cryptowatcher.Adapters.TokenAdapter
 import com.andyisdope.cryptowatcher.model.Tokens
+import java.util.*
+import android.content.DialogInterface
+import android.support.v7.app.AlertDialog
+import kotlin.Comparator
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val SIGNIN_REQUEST = 1001
-    val MY_GLOBAL_PREFS = "my_global_prefs"
-    private val TAG = "MainActivity"
-    //var dataItemList = SampleDataProvider.dataItemList
 
-
-    //var mDataSource: DataSource
+    var Ascending: Comparator<Currency>? = null
     var mCoins: ArrayList<Currency>? = ArrayList()
     var mTokens: ArrayList<Tokens>? = ArrayList()
     var mCoinList: RecyclerView? = null
@@ -44,6 +43,11 @@ class MainActivity : AppCompatActivity() {
     val READ_STORAGE_PERMISSION_REQUEST_CODE = 1
     var response: String = ""
     var response2: String = ""
+    var TimeFrames: Array<String>? = null
+    var SortBy: Array<String>? = null
+    var Order: Array<String>? = null
+
+
 
     private val mBroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
@@ -122,6 +126,16 @@ class MainActivity : AppCompatActivity() {
         val settings = PreferenceManager.getDefaultSharedPreferences(this)
         val grid = false
         initTabs()
+
+        Order = arrayOf("Ascending", "Descending")
+        TimeFrames = arrayOf("Hourly", "Daily", "Weekly")
+        SortBy = arrayOf("Place", "Alphabet", "Price", "24Hr Volume", "MarketCap")
+        Currency.SortMethod = "Place"
+        Currency.TimeFrame = "Hourly"
+
+        Tokens.SortMethod = "Place"
+        Tokens.TimeFrame = "Hourly"
+
         mCoinList = findViewById<RecyclerView>(R.id.CoinList) as RecyclerView
         mTokenList = findViewById<RecyclerView>(R.id.TokenList) as RecyclerView
         mFavourites = findViewById<RecyclerView>(R.id.Favorites) as RecyclerView
@@ -141,6 +155,97 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, DataService::class.java)
         intent.putExtra("Path", path)
         startService(intent)
+    }
+
+    private fun sortAdapters(method: String) {
+        Currency.SortMethod = method
+        Tokens.SortMethod = method
+        when (method) {
+            "Hourly" -> {
+                Tokens.TimeFrame = method
+                Currency.TimeFrame = method
+            }
+            "Daily" -> {
+                Tokens.TimeFrame = method
+                Currency.TimeFrame = method
+            }
+            "Weekly" -> {
+                Tokens.TimeFrame = method
+                Currency.TimeFrame = method
+            }
+            "Price" -> {
+                when(Currency.Order)
+                {
+                   "Ascending" -> {
+                       mCoins!!.sortBy { it.CurrentPrice.toDouble() }
+                       mTokens!!.sortBy { it.CurrentPrice.toDouble() }
+                   }
+                    "Descending" -> {
+                        mCoins!!.sortByDescending { it.CurrentPrice.toDouble() }
+                        mTokens!!.sortByDescending { it.CurrentPrice.toDouble() }
+                    }
+                }
+            }
+            "Place" -> {
+                when(Currency.Order)
+                {
+                    "Ascending" -> {
+                        mCoins!!.sortBy { it.Place }
+                        mTokens!!.sortBy { it.Place }
+                    }
+                    "Descending" -> {
+                        mCoins!!.sortByDescending { it.Place }
+                        mTokens!!.sortByDescending { it.Place }
+                    }
+                }
+            }
+            "24Hr Volume" -> {
+                when(Currency.Order)
+                {
+                    "Ascending" -> {
+                        mCoins!!.sortBy { it.Volume.toDouble() }
+                        mTokens!!.sortBy { it.Volume.toDouble() }
+                    }
+                    "Descending" -> {
+                        mCoins!!.sortByDescending { it.Volume.toDouble() }
+                        mTokens!!.sortByDescending { it.Volume.toDouble() }
+                    }
+                }
+            }
+            "MarketCap" -> {
+                when(Currency.Order)
+                {
+                    "Ascending" -> {
+                        mCoins!!.sortBy { it.MarketCap.toDouble() }
+                        mTokens!!.sortBy { it.MarketCap.toDouble() }
+                    }
+                    "Descending" -> {
+                        mCoins!!.sortByDescending { it.MarketCap.toDouble() }
+                        mTokens!!.sortByDescending { it.MarketCap.toDouble() }
+                    }
+                }
+            }
+            "Alphabet" -> {
+                when(Currency.Order)
+                {
+                    "Ascending" -> {
+                        mCoins!!.sortBy { it.Name }
+                        mTokens!!.sortBy { it.Name }
+                    }
+                    "Descending" -> {
+                        mCoins!!.sortByDescending { it.Name }
+                        mTokens!!.sortByDescending { it.Name }
+                    }
+                }
+            }
+        }
+        mTokenAdapter = TokenAdapter(this, mTokens!!)
+        mTokenList!!.adapter = mTokenAdapter
+        mTokenList!!.adapter.notifyDataSetChanged()
+
+        mCoinAdapter = CurrencyAdapter(this, mCoins!!)
+        mCoinList!!.adapter = mCoinAdapter
+        mCoinList!!.adapter.notifyDataSetChanged()
     }
 
     private fun displayTokenItems() {
@@ -165,7 +270,6 @@ class MainActivity : AppCompatActivity() {
         blocks.take(894)
                 .filter { it.length > 19 }
                 .forEach { mCoins!!.add(createCurrency(it)) }
-
 
         mCoinList!!.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         mCoinAdapter = CurrencyAdapter(this, mCoins!!)
@@ -209,20 +313,38 @@ class MainActivity : AppCompatActivity() {
         tabs.addTab(spec)
     }
 
+    fun createBuilderDialog(array: Array<String>?, title: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setItems(array, { dialog, which ->
+            sortAdapters(array!![which])
+        })
+        builder.show()
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         //sorts: alphabet, marketcap, biggest change, price, place
         when (item.itemId) {
             R.id.search -> Toast.makeText(this, "Search selected", Toast.LENGTH_SHORT).show()
             R.id.sort -> {
-
-
-
-
-                Toast.makeText(this, "Sort selected", Toast.LENGTH_SHORT).show()
+                createBuilderDialog(SortBy, "Select a criteria to sort on")
             }
-            R.id.currency -> Toast.makeText(this, "Currency selected", Toast.LENGTH_SHORT).show()
-            R.id.time -> Toast.makeText(this, "Time selected", Toast.LENGTH_SHORT).show()
+            R.id.currency -> {
+            }
+            R.id.time -> {
+                createBuilderDialog(TimeFrames, "Select a Percent Change Period to display")
+            }
+            R.id.order ->
+            {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Select an order direction")
+                builder.setItems(Order, { _, which ->
+                    Currency.Order = Order!![which]
+                    Tokens.Order = Order!![which]
+                    Toast.makeText(baseContext, "Current order: ${Currency.Order}",Toast.LENGTH_SHORT).show()
+                })
+                builder.show()
+            }
         }
         return true
     }
@@ -232,6 +354,7 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.actionmenu, menu)
         return true
     }
+
 
     private fun createCurrency(block: String): Currency {
         var id = block.substringAfter("<tr id=\"id-")
@@ -244,13 +367,16 @@ class MainActivity : AppCompatActivity() {
         place = place.substring(0, place.indexOf("<")).replace("\n", "").trim()
 
         var marketCap = block.substringAfter("class=\"no-wrap market-cap text-right\" data-usd=\"")
-        marketCap = marketCap.substring(0, marketCap.indexOf("\""))
+        marketCap = marketCap.substring(0, marketCap.indexOf("\"")).toUpperCase()
+       // if(marketCap.contains("+")) marketCap = marketCap.replace("+", "")
 
         var currPrice = block.substringAfter("class=\"price\" data-usd=\"")
-        currPrice = currPrice.substring(0, currPrice.indexOf("\""))
+        currPrice = currPrice.substring(0, currPrice.indexOf("\"")).toUpperCase()
+        //if(currPrice.contains("+")) currPrice = currPrice.replace("+", "")
 
         var volume = block.substringAfter("class=\"volume\" data-usd=\"")
-        volume = volume.substring(0, volume.indexOf("\""))
+        volume = volume.substring(0, volume.indexOf("\"")).toUpperCase()
+        //if(volume.contains("+")) volume = volume.replace("+", "")
 
         var hrChange = block.substringAfter("no-wrap percent-1h").substringAfter("data-usd=\"")
         hrChange = hrChange.substring(0, hrChange.indexOf("\""))
@@ -261,7 +387,14 @@ class MainActivity : AppCompatActivity() {
         var sevenChange = block.substringAfter("no-wrap percent-7d").substringAfter("data-usd=\"")
         sevenChange = sevenChange.substring(0, sevenChange.indexOf("\""))
 
-        return Currency(id, symbol, Integer.parseInt(place), marketCap, currPrice, hrChange, "1hr")
+            if(marketCap == "?") marketCap = "-9999"
+            if(currPrice == "?") currPrice = "-9999"
+            if(hrChange == "?")  hrChange = "-9999"
+            if(twoChange == "?") twoChange = "-9999"
+            if(sevenChange == "?") sevenChange = "-9999"
+            if(volume == "?" || volume == "NONE") volume = "-9999"
+
+        return Currency(id, symbol, Integer.parseInt(place), marketCap, currPrice, hrChange, twoChange, sevenChange, volume)
     }
 
     private fun createToken(block: String): Tokens {
@@ -278,13 +411,16 @@ class MainActivity : AppCompatActivity() {
         platform = platform.substring(0, platform.indexOf("\""))
 
         var marketCap = block.substringAfter("class=\"no-wrap market-cap text-right\" data-usd=\"")
-        marketCap = marketCap.substring(0, marketCap.indexOf("\""))
+        marketCap = marketCap.substring(0, marketCap.indexOf("\"")).toUpperCase()
+        //if(marketCap.contains("+")) marketCap = marketCap.replace("+", "")
 
         var currPrice = block.substringAfter("class=\"price\" data-usd=\"")
-        currPrice = currPrice.substring(0, currPrice.indexOf("\""))
+        currPrice = currPrice.substring(0, currPrice.indexOf("\"")).toUpperCase()
+        //if(currPrice.contains("+")) currPrice = currPrice.replace("+", "")
 
         var volume = block.substringAfter("class=\"volume\" data-usd=\"")
-        volume = volume.substring(0, volume.indexOf("\""))
+        volume = volume.substring(0, volume.indexOf("\"")).toUpperCase()
+        //if(volume.contains("+")) volume = volume.replace("+", "")
 
         var hrChange = block.substringAfter("no-wrap percent-1h").substringAfter("data-usd=\"")
         hrChange = hrChange.substring(0, hrChange.indexOf("\""))
@@ -295,6 +431,13 @@ class MainActivity : AppCompatActivity() {
         var sevenChange = block.substringAfter("no-wrap percent-7d").substringAfter("data-usd=\"")
         sevenChange = sevenChange.substring(0, sevenChange.indexOf("\""))
 
-        return Tokens(id, symbol, Integer.parseInt(place), platform, marketCap, currPrice, hrChange, "1hr")
+        if(marketCap == "?") marketCap = "-9999"
+        if(currPrice == "?") currPrice = "-9999"
+        if(hrChange == "?")  hrChange = "-9999"
+        if(twoChange == "?") twoChange = "-9999"
+        if(sevenChange == "?") sevenChange = "-9999"
+        if(volume == "?" || volume == "NONE") volume = "-9999"
+
+        return Tokens(id, symbol, Integer.parseInt(place), platform, marketCap, currPrice, hrChange, twoChange, sevenChange, volume)
     }
 }
